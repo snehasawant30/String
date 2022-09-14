1c1321bf1fa32654d1df2633467a3c79d1cf2cee
snehasawant30 <sneha.sawant778@gmail.com>
2022-09-14 11:51:56 +0530

    Count Characters

---
+++ String/src/com/sneha/string/CountChar.java
@@ -0,0 +1,26 @@
+package com.sneha.string;
+
+import java.util.HashMap;
+
+public class CountChar {
+
+	public static void main(String[] args) {
+		String name = "Hello World";
+		int count;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for(int i = 0; i < name.length(); i++) {
+			count = 0;
+			char ch = name.charAt(i);
+			for (int j = 0; j < name.length(); j++) {
+				if(ch == name.charAt(j)) {
+					count++;
+				}
+			}
+			if(ch != ' ') {
+				map.put(ch, count);
+			}
+		}
+		System.out.println(map);
+	}
+
+}
