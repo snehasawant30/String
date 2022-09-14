1feb8a5831582187e72322b5dd9cf22c93512816
snehasawant30 <sneha.sawant778@gmail.com>
2022-09-14 15:50:38 +0530

    Not Anagram logic

--- String/src/com/sneha/string/AnagramString.java
+++ String/src/com/sneha/string/AnagramString.java
@@ -5,9 +5,11 @@ public class AnagramString {
 
 	public static void main(String[] args) {
 		String str1 = "Read";
-		String str2 = "Dear";
-		int count = 0;
+		String str2 = "bear";
+		String str3 = "Dear";
+		int count;
 		if (str1.length() == str2.length()) {
+			count = 0;
 			for (int i = 0; i < str1.length(); i++) {
 				char ch1 = str1.toLowerCase().charAt(i);
 				for (int j=0; j < str2.length(); j++) {
@@ -20,10 +22,32 @@ public class AnagramString {
 			if(count == str1.length()) {
 				System.out.println(str1+" and "+str2+" are Anagram");
 			}
+			else {
+				System.out.println(str1+" and "+str2+" are not Anagram");
+			}
+		}
+		else {
+			System.out.println(str1+" and "+str2+" are not Anagram");	
+		}
+		
+		if (str1.length() == str3.length()) {
+			count = 0;
+			for (int i = 0; i < str1.length(); i++) {
+				char ch1 = str1.toLowerCase().charAt(i);
+				for (int j=0; j < str3.length(); j++) {
+					char ch2 = str3.toLowerCase().charAt(j);
+					if(ch1 == ch2) {
+						count++;
+					}
+				}
+			}
+			if(count == str1.length()) {
+				System.out.println(str1+" and "+str3+" are Anagram");
+			}
+			else
+				System.out.println(str1+" and "+str3+" are not Anagram");
 		}
 		else
-			System.out.println(str1+" and "+str2+" are not Anagram");
-		
+			System.out.println(str1+" and "+str3+" are not Anagram");
 	}
-
 }
