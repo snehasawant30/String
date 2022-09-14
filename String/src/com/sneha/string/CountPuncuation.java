944d5c1209c7fe4a17fdbf59f618925c2d019b02
snehasawant30 <sneha.sawant778@gmail.com>
2022-09-14 13:40:18 +0530

    logic

--- String/src/com/sneha/string/CountPuncuation.java
+++ String/src/com/sneha/string/CountPuncuation.java
@@ -1,10 +1,18 @@
 package com.sneha.string;
-
+//3) Java Program to count the total number of punctuation characters exists in a String
 public class CountPuncuation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String name = "He said, Why do you come and disturb me? What a nuisance you are! Why can’t you play somewhere else? "
+				+ "Can’t you see that I want to work? Go away at once and do not come here again.";
+		int count = 0;
+		for(int i = 0; i< name.length(); i++) {
+			char ch = name.charAt(i);
+			if(ch == '!' || ch == ',' || ch == ';' || ch == '.' || ch == '-' || ch == '"' || ch == '?' || ch == ';' || ch == '/') {
+				count++;
+				System.out.print(" "+ch);
+			}
+		}
+		System.out.println(count);
 	}
-
 }
